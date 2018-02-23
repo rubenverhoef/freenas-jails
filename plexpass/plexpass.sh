1bb07821bf5e1efcd0fad18f1e5e601519399729
rubenverhoef <ruben_verhoef@hotmail.com>
2018-02-23 16:03:24 +0100

    Plex backup now working and plex updates itself to latest version on installation

--- plexpass/plexpass.sh
+++ plexpass/plexpass.sh
@@ -3,6 +3,14 @@
 . $(dirname $0)/plexpass_config.sh
 . $(dirname $0)/config.sh
 
+mkdir -p /usr/local/etc/pkg/repos
+echo "FreeBSD: {" >> "/usr/local/etc/pkg/repos/FreeBSD.conf"
+echo "    url: \"pkg+http://pkg.FreeBSD.org/\${ABI}/latest\"" >> "/usr/local/etc/pkg/repos/FreeBSD.conf"
+echo "}" >> "/usr/local/etc/pkg/repos/FreeBSD.conf"
+
+pkg update -f
+pkg upgrade -y
+
 #create user
 pw useradd -n $USER_NAME -u $USER_ID -d /nonexistent -s /usr/sbin/nologin
 
