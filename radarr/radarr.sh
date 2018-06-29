020369c2d4535ee5e659d7d9e07665c9a3c35c75
rubenverhoef <ruben_verhoef@hotmail.com>
2018-06-29 21:12:42 +0200

    Fix radarr installation

--- radarr/radarr.sh
+++ radarr/radarr.sh
@@ -15,14 +15,15 @@ chown -R $USER_NAME:$USER_NAME /usr/local/share/radarr/
 sysrc 'radarr_user='$USER_NAME''
 echo "permissions set"
 
-rm -R /usr/local/sonarr
+rm -R /usr/local/radarr
 
 service radarr start
 sleep 10
 
 if [ -z "$radarr_SUB_DOMAIN" ]; then
+	echo "Change URL Base"
 	sed -i '' -e 's,<UrlBase></UrlBase>,<UrlBase>radarr</UrlBase>,g' /usr/local/radarr/config.xml
 fi
 sed -i '' -e 's,<Port>7878</Port>,<Port>'$radarr_PORT'</Port>,g' /usr/local/radarr/config.xml
 
-service radarr start
+service radarr restart
