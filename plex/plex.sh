4667ddb5a54f11e6db9e522da7b0a0186dc5d9eb
rubenverhoef <ruben_verhoef@hotmail.com>
2018-06-29 19:54:23 +0200

    Plex iocage plugin support (custom because wanted latest version)

--- plex/plex.sh
+++ plex/plex.sh
@@ -3,22 +3,18 @@
 . $(dirname $0)/plex_config.sh
 . $(dirname $0)/config.sh
 
-mkdir -p /usr/local/etc/pkg/repos
-echo "FreeBSD: {" >> "/usr/local/etc/pkg/repos/FreeBSD.conf"
-echo "    url: \"pkg+http://pkg.FreeBSD.org/\${ABI}/latest\"" >> "/usr/local/etc/pkg/repos/FreeBSD.conf"
-echo "}" >> "/usr/local/etc/pkg/repos/FreeBSD.conf"
-
-pkg update -f
-pkg upgrade -y
+service plexmediaserver stop
 
 #create user
 pw useradd -n $USER_NAME -u $USER_ID -d /nonexistent -s /usr/sbin/nologin
 
-mkdir -p /usr/local/plexdata-plexpass/Plex\ Media\ Server/Mounts/
-ln -s /mnt/* /usr/local/plexdata-plexpass/Plex\ Media\ Server/Mounts/
-chown -R $USER_NAME:$USER_NAME /usr/local/plexdata-plexpass
+mkdir -p /usr/local/plexdata/Plex\ Media\ Server/Mounts/
+ln -s /mnt/* /usr/local/plexdata/Plex\ Media\ Server/Mounts/
+chown -R $USER_NAME:$USER_NAME /usr/local/plexdata
+chown -R $USER_NAME:$USER_NAME /var/run/plex
 
-sysrc 'ifconfig_epair0_name=epair0b'
-sysrc 'plexmediaserver_enable=YES'
-sysrc 'plexmediaserver_user='$USER_NAME''
-sysrc 'plexmediaserver_group='$USER_NAME''
+#sysrc 'ifconfig_epair0_name=epair0b'
+sysrc 'plexmediaserver_user=jailuser'
+sysrc 'plexmediaserver_group=jailuser'
+
+service plexmediaserver start
--- plexpass/plexpass.sh
+++ plexpass/plexpass.sh
@@ -3,13 +3,7 @@
 . $(dirname $0)/plexpass_config.sh
 . $(dirname $0)/config.sh
 
-mkdir -p /usr/local/etc/pkg/repos
-echo "FreeBSD: {" >> "/usr/local/etc/pkg/repos/FreeBSD.conf"
-echo "    url: \"pkg+http://pkg.FreeBSD.org/\${ABI}/latest\"" >> "/usr/local/etc/pkg/repos/FreeBSD.conf"
-echo "}" >> "/usr/local/etc/pkg/repos/FreeBSD.conf"
-
-pkg update -f
-pkg upgrade -y
+service plexmediaserver_plexpass stop
 
 #create user
 pw useradd -n $USER_NAME -u $USER_ID -d /nonexistent -s /usr/sbin/nologin
@@ -17,8 +11,10 @@ pw useradd -n $USER_NAME -u $USER_ID -d /nonexistent -s /usr/sbin/nologin
 mkdir -p /usr/local/plexdata-plexpass/Plex\ Media\ Server/Mounts/
 ln -s /mnt/* /usr/local/plexdata-plexpass/Plex\ Media\ Server/Mounts/
 chown -R $USER_NAME:$USER_NAME /usr/local/plexdata-plexpass
+chown -R $USER_NAME:$USER_NAME /var/run/plex
 
-sysrc 'ifconfig_epair0_name=epair0b'
-sysrc 'plexmediaserver_plexpass_enable=YES'
+#sysrc 'ifconfig_epair0_name=epair0b'
 sysrc 'plexmediaserver_plexpass_user='$USER_NAME''
 sysrc 'plexmediaserver_plexpass_group='$USER_NAME''
+
+service plexmediaserver_plexpass start
