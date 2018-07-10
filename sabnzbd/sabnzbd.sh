fd3774e57c3ac05c3734d9552b5f807d5a51f300
rubenverhoef <ruben_verhoef@hotmail.com>
2018-07-10 20:00:04 +0200

    fix sabnzbd openssl error and config sed error

--- sabnzbd/sabnzbd.sh
+++ sabnzbd/sabnzbd.sh
@@ -15,9 +15,9 @@ chown -R $USER_NAME:$USER_NAME /var/run/sabnzbd
 sysrc 'sabnzbd_user='$USER_NAME''
 sysrc 'sabnzbd_group='$USER_NAME''
 
-sed -i '' -e 's,port = 8080,port = '$sabnzbd_PORT',g' /.sabnzbd/sabnzbd.ini
-sed -i '' -e 's,host = 127.0.0.1,host = 0.0.0.0,g' /.sabnzbd/sabnzbd.ini
-sed -i '' -e 's,download_dir = Downloads/incomplete,download_dir = //mnt/downloads/incomplete,g' /.sabnzbd/sabnzbd.ini
-sed -i '' -e 's,complete_dir = Downloads/complete,complete_dir = //mnt/downloads/complete,g' /.sabnzbd/sabnzbd.ini
+sed -i '' -e 's,port = 8080,port = '$sabnzbd_PORT',g' /usr/local/sabnzbd/sabnzbd.ini
+sed -i '' -e 's,host = 127.0.0.1,host = 0.0.0.0,g' /usr/local/sabnzbd/sabnzbd.ini
+sed -i '' -e 's,download_dir = Downloads/incomplete,download_dir = //mnt/downloads/incomplete,g' /usr/local/sabnzbd/sabnzbd.ini
+sed -i '' -e 's,complete_dir = Downloads/complete,complete_dir = //mnt/downloads/complete,g' /usr/local/sabnzbd/sabnzbd.ini
 
 service sabnzbd start
