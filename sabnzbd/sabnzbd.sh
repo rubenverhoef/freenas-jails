2fab2d6d02b25010f76b74730b8a930a1ac25371
rubenverhoef <ruben_verhoef@hotmail.com>
2018-09-30 19:50:11 +0200

    change sabnzbd back to manual installation to get latest version

--- sabnzbd/sabnzbd.sh
+++ sabnzbd/sabnzbd.sh
@@ -8,16 +8,21 @@ service sabnzbd stop
 #create user
 pw useradd -n $USER_NAME -u $USER_ID -d /nonexistent -s /usr/sbin/nologin
 
-mkdir -p /usr/local/sabnzbd
-chown -R $USER_NAME:$USER_NAME /usr/local/sabnzbd
-chown -R $USER_NAME:$USER_NAME /var/run/sabnzbd
+rm -R /.sabnzbd
+mkdir /.sabnzbd
+chown $USER_NAME:$USER_NAME /.sabnzbd
+chown $USER_NAME:$USER_NAME /var/run/sabnzbd
 
 sysrc 'sabnzbd_user='$USER_NAME''
 sysrc 'sabnzbd_group='$USER_NAME''
 
-sed -i '' -e 's,port = 8080,port = '$sabnzbd_PORT',g' /usr/local/sabnzbd/sabnzbd.ini
-sed -i '' -e 's,host = 127.0.0.1,host = 0.0.0.0,g' /usr/local/sabnzbd/sabnzbd.ini
-sed -i '' -e 's,download_dir = Downloads/incomplete,download_dir = //mnt/downloads/incomplete,g' /usr/local/sabnzbd/sabnzbd.ini
-sed -i '' -e 's,complete_dir = Downloads/complete,complete_dir = //mnt/downloads/complete,g' /usr/local/sabnzbd/sabnzbd.ini
+service sabnzbd start
+sleep 10
+service sabnzbd stop
+
+sed -i '' -e 's,port = 8080,port = '$sabnzbd_PORT',g' /.sabnzbd/sabnzbd.ini
+sed -i '' -e 's,host = 127.0.0.1,host = 0.0.0.0,g' /.sabnzbd/sabnzbd.ini
+sed -i '' -e 's,download_dir = Downloads/incomplete,download_dir = //mnt/downloads/incomplete,g' /.sabnzbd/sabnzbd.ini
+sed -i '' -e 's,complete_dir = Downloads/complete,complete_dir = //mnt/downloads/complete,g' /.sabnzbd/sabnzbd.ini
 
 service sabnzbd start
