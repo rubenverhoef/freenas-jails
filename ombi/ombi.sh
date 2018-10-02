4e97458b21fc3d6ed9305ceaa7cf235bb66e0d4b
rubenverhoef <ruben_verhoef@hotmail.com>
2018-10-02 22:59:17 +0200

    ombi update

--- ombi/ombi.sh
+++ ombi/ombi.sh
@@ -5,3 +5,18 @@
 
 #create user
 pw useradd -n $USER_NAME -u $USER_ID -d /nonexistent -s /usr/sbin/nologin
+
+sysrc 'ombi_enable=YES'
+sysrc 'ombi_user='$USER_NAME''
+sysrc 'ombi_group='$USER_NAME''
+sysrc 'ombi_data_dir="/usr/local/ombi"'
+sysrc 'ombi_pidfile="/var/run/ombi/ombi.pid"'
+
+rm -rf /usr/local/ombi
+mkdir /usr/local/ombi
+mkdir /var/run/ombi
+
+chown $USER_NAME:$USER_NAME /usr/local/ombi
+chown $USER_NAME:$USER_NAME /var/run/ombi
+
+service ombi start
