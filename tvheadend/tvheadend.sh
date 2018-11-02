7f4e7c09ad69e63118d45a96baaa01c764a31a9c
rubenverhoef <ruben_verhoef@hotmail.com>
2018-11-02 19:32:26 +0100

    [Tvheadend] Change permissions because we changed user

--- tvheadend/tvheadend.sh
+++ tvheadend/tvheadend.sh
@@ -11,4 +11,7 @@ pw useradd -n $USER_NAME -u $USER_ID -d /nonexistent -s /usr/sbin/nologin
 sysrc 'tvheadend_user='$USER_NAME''
 sysrc 'tvheadend_group='$USER_NAME''
 
+chown -R $USER_NAME:$USER_NAME /usr/local/etc/tvheadend
+chown -R $USER_NAME:$USER_NAME /var/log/tvheadend
+
 service tvheadend start
