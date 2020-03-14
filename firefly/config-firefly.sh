7cc5497dbf95a804385d1d88e724606deb3ee554
Ruben Verhoef <ruben_verhoef@hotmail.com>
2020-03-14 20:40:14 +0100

    forgotten to apply jail name

--- firefly/config-firefly.sh
+++ firefly/config-firefly.sh
@@ -3,11 +3,11 @@
 . $(dirname $0)/firefly_config.sh
 . $(dirname $0)/../config.sh
 
-iocage set -P firefly-iii=SITE_OWNER=$EMAIL_ADDRESS
-iocage set -P firefly-iii=APP_URL=$firefly_SUB_DOMAIN.$DOMAIN
-iocage set -P firefly-iii=DB_CONNECTION=mysql
-iocage set -P firefly-iii=DB_HOST=$webserver_IP
-iocage set -P firefly-iii=DB_PORT=3306
-iocage set -P firefly-iii=DB_DATABASE=$firefly_MYSQL_DATABASE
-iocage set -P firefly-iii=DB_USERNAME=$firefly_MYSQL_USERNAME
-iocage set -P firefly-iii=DB_PASSWORD=$firefly_MYSQL_PASSWORD
+iocage set -P firefly-iii=SITE_OWNER=$EMAIL_ADDRESS firefly
+iocage set -P firefly-iii=APP_URL=$firefly_SUB_DOMAIN.$DOMAIN firefly
+iocage set -P firefly-iii=DB_CONNECTION=mysql firefly
+iocage set -P firefly-iii=DB_HOST=$webserver_IP firefly
+iocage set -P firefly-iii=DB_PORT=3306 firefly
+iocage set -P firefly-iii=DB_DATABASE=$firefly_MYSQL_DATABASE firefly
+iocage set -P firefly-iii=DB_USERNAME=$firefly_MYSQL_USERNAME firefly
+iocage set -P firefly-iii=DB_PASSWORD=$firefly_MYSQL_PASSWORD firefly
