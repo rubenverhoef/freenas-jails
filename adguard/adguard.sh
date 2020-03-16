d83bd6e1f17347cdea3d071d08aaa2907e8279fa
Ruben Verhoef <ruben_verhoef@hotmail.com>
2020-03-16 19:40:12 +0100

    remove unused adguard script

--- adguard/adguard.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-. $(dirname $0)/adguard_config.sh
-. $(dirname $0)/config.sh
-
-#create user
-pw useradd -n $USER_NAME -u $USER_ID -d /nonexistent -s /usr/sbin/nologin
-
-service adguard restart
