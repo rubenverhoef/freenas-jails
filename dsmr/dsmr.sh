ef0691271582a3be4d032db31e96297d63114eb3
Ruben Verhoef <ruben_verhoef@hotmail.com>
2020-11-15 17:35:27 +0100

    [DSMR] add support vor V4.x

--- dsmr/dsmr.sh
+++ dsmr/dsmr.sh
@@ -2,3 +2,6 @@
 
 . $(dirname $0)/dsmr_config.sh
 . $(dirname $0)/config.sh
+
+sed -i '' -e 's,#!/bin/bash*,#!/usr/local/bin/bash,g' /home/dsmr/dsmr-reader/post-deploy.sh
+ln -s /usr/local/bin/python3.7 /usr/local/bin/python3
