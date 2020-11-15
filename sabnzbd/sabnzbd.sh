0f3330e2fd60d21df77fe4a84cbffc45e680272c
Ruben Verhoef <ruben_verhoef@hotmail.com>
2020-11-15 15:49:08 +0100

    [SABnzbd] upgrade install script to SABnzbd >= 3.0

--- sabnzbd/sabnzbd.sh
+++ sabnzbd/sabnzbd.sh
@@ -3,7 +3,7 @@
 . $(dirname $0)/sabnzbd_config.sh
 . $(dirname $0)/config.sh
 
-sed -i '' -e 's,#!/usr/bin/python*,#!/usr/local/bin/python2.7,g' /root/SABnzbd/SABnzbd.py
+sed -i '' -e 's,#!/usr/bin/python3*,#!/usr/local/bin/python3.7,g' /root/SABnzbd/SABnzbd.py
 
 sysrc 'sabnzbd_enable=YES'
 sysrc 'sabnzbd_user='$USER_NAME''
@@ -22,7 +22,6 @@ service sabnzbd start
 sleep 10
 service sabnzbd stop
 
-sed -i '' -e 's,port = 8080,port = '$sabnzbd_PORT',g' /usr/local/sabnzbd/sabnzbd.ini
 sed -i '' -e 's,host = 127.0.0.1,host = 0.0.0.0,g' /usr/local/sabnzbd/sabnzbd.ini
 sed -i '' -e 's,download_dir = Downloads/incomplete,download_dir = //mnt/downloads/incomplete,g' /usr/local/sabnzbd/sabnzbd.ini
 sed -i '' -e 's,complete_dir = Downloads/complete,complete_dir = //mnt/downloads/complete,g' /usr/local/sabnzbd/sabnzbd.ini
